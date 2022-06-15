038cdaac865ebd35f9bb889a697b42afe2f26536
applegammer <applegammer@gmail.com>
2022-06-15 17:02:10 +0200

    change graphic name on 3 line of code

--- converter_rgb332.py
+++ converter_rgb332.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-im = Image.open('black_white.png', 'r')
+im = Image.open('bw_template.png', 'r')
 width, height = im.size
 pixel_values = list(im.getdata())
 pix = im.load()
