10be3a4dfe61bc164c614cbc83f6a1ab775e1350
applegammer <applegammer@gmail.com>
2022-06-16 20:57:21 +0200

    removed unused code, optimizing

--- converter_rgb332.py
+++ converter_rgb332.py
@@ -5,10 +5,7 @@ im = Image.open('bw_template.png', 'r')     # Function opens an image from the d
 width, height = im.size                     # Defined two values witch will be use on the loop to check  pixel by pixel and it take width and height from an image
 pixel_map = im.load()                       # Check lines X,Y and get values from an image (e.g pixel 0,0 give 255, 255, 255 RGB palette)
 
-# print (width, height)
-# print (pixel_values)
-# print (pixel_map)
-
+#Program section
 rgb_output = open('test_output.txt', 'w')                                       # Value which create a text file, where will be store output data
 for x in range (width):                                                         # For x lines from 0 to end height of an image
     for y in range (height):                                                    # For y lines from 0 to end width of an image
