4e9c91111f148f3270753a48cdb4b4851576b5e5
applegammer <applegammer@gmail.com>
2022-06-16 15:38:58 +0200

    added function - write output(values) from loop to file

--- converter_rgb332.py
+++ converter_rgb332.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import sys
 
 im = Image.open('bw_template.png', 'r')
 width, height = im.size
@@ -9,8 +10,11 @@ pixel_map = im.load()
 # print (pixel_values)
 # print (pixel_map)
 
+rgb_output = open('test_output.txt', 'w')
 for x in range (width):
     for y in range (height):
         print ("position of X: " + str(x) + " position of Y: " + str(y))
-        rgb = pixel_map [x, y]
-        print (rgb)
+        rgb24 = pixel_map [x, y]
+        print (rgb24)
+        rgb_output.write(str(rgb24))
+rgb_output.close()
