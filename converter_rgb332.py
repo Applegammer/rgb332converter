2799b80ecbd786dded7538f4a42a128b41f719bd
applegammer <applegammer@gmail.com>
2022-06-16 20:23:16 +0200

    remove unused value, add comments to program

--- converter_rgb332.py
+++ converter_rgb332.py
@@ -1,20 +1,19 @@
 from PIL import Image
-import sys
 
-im = Image.open('bw_template.png', 'r')
-width, height = im.size
-pixel_values = list(im.getdata())
-pixel_map = im.load()
+#Part of function from PILLOW library
+im = Image.open('bw_template.png', 'r')     # Function opens an image from the defined path
+width, height = im.size                     # Defined two values witch will be use on the loop to check  pixel by pixel and it take width and height from an image
+pixel_map = im.load()                       # Check lines X,Y and get values from an image (e.g pixel 0,0 give 255, 255, 255 RGB palette)
 
 # print (width, height)
 # print (pixel_values)
 # print (pixel_map)
 
-rgb_output = open('test_output.txt', 'w')
-for x in range (width):
-    for y in range (height):
-        print ("position of X: " + str(x) + " position of Y: " + str(y))
-        rgb24 = pixel_map [x, y]
-        print (rgb24)
-        rgb_output.write(str(rgb24) + "," )
-rgb_output.close()
+rgb_output = open('test_output.txt', 'w')                                       # Value which create a text file, where will be store output data
+for x in range (width):                                                         # For x lines from 0 to end height of an image
+    for y in range (height):                                                    # For y lines from 0 to end width of an image
+        print ("position of X: " + str(x) + " position of Y: " + str(y))        # Values to debugging if loop works properly
+        rgb24 = pixel_map [x, y]                                                # Value store RGB palett
+        print (rgb24)                                                           # Values to debbugging if RGB output from pixels are correctly
+        rgb_output.write(str(rgb24) + "," )                                     # Store output values to a file
+rgb_output.close()                                                              # Close file after loop end
