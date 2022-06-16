6df331a7ec0214efcbe56bbf4cb9775570ab4013
applegammer <applegammer@gmail.com>
2022-06-16 13:57:18 +0200

    check functions from PIL library - tests

--- converter_rgb332.py
+++ converter_rgb332.py
@@ -3,8 +3,8 @@ from PIL import Image
 im = Image.open('bw_template.png', 'r')
 width, height = im.size
 pixel_values = list(im.getdata())
-pix = im.load()
+pixel_map = im.load()
 
 print (width, height)
 print (pixel_values)
-print (pix)
+print(pixel_map)
